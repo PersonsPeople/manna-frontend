3cbe7fba447f40f586c4043c01deddac25ef56e0
spammy <spammy7147@gmail.com>
2021-05-11 20:14:22 +0900

    reset.css cdn 적용

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,6 +11,7 @@ export default {
       { hid: 'description', name: 'description', content: '' },
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'stylesheet', href: 'https://cdn.jsdelivr.net/npm/reset-css@5.0.1/reset.min.css' }],
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
